26de482f256664caf243596d39e8b65820499b0b
R. M. Mindaugas <rmmind8s@gmail.com>
2025-07-05 10:05:26 +0300

    qq

--- public_html/index.php
+++ public_html/index.php
@@ -117,7 +117,7 @@ if (strlen($short) > 0) {
           <p class="mt-1"><?= $short ?></p>
         </div>
       </div>
-      <h1 class="text-3xl font-semibold text-gray-900 flex items-center"><?= $phone ?> <a id="copy" href="#" onclick="copyToClipboard('<?= $phone ?>');return false;" class="text-gray-500 text-xl"><i class="fa-solid fa-copy"></i></a></h1>
+      <h1 class="text-3xl font-semibold text-gray-900 flex items-center"><?= $phone ?> <a id="copy" href="#" onclick="copyToClipboard('<?= $phone ?>');return false;" class="text-gray-700 ml-2 text-xl"><i class="fa-solid fa-copy"></i></a></h1>
       <!-- Skambinimo mygtukas -->
       <button
         class="text-green-500 flex items-center justify-center text-7xl mt-10"
