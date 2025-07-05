e0d4672263097311ff6df856deccb0ae44f533e7
R. M. Mindaugas <rmmind8s@gmail.com>
2025-07-05 21:36:41 +0300

    btn register

--- public_html/index.php
+++ public_html/index.php
@@ -30,7 +30,7 @@ $data = array(
   )
 );
 
-$description = "Telefono numeris nėra registruotas sistemoje, todėl papildoma informacija gali būti netiksli. Jeigu esate šio numerio savininkas arba valdytojas, tuomet galite jį <a class='text-gree-500 underline' href='sms:+37061600055?body=Prašau užregistruoti mano telefono numerį sistemoje Skambink.TEL'>užregistruoti</a>.";
+$description = "Telefono numeris nėra registruotas sistemoje, todėl papildoma informacija gali būti netiksli. Jeigu esate šio numerio savininkas arba valdytojas, tuomet galite jį:<br><a class='mt-4 px-6 py-2 bg-blue-500 text-white rounded-xl hover:bg-blue-600' href='sms:+37061600055?body=Prašau užregistruoti mano telefono numerį sistemoje Skambink.TEL'>Užregistruoti</a>.";
 $image = "https://skambink.tel/img/default.png";
 
 if( isset($data[$phone]) ) {
@@ -163,6 +163,9 @@ if (strlen($short) > 0) {
         </button>
 
         <p class="text-gray-600" style="margin-top:0;" id="modalMessage"><?= $description ?></p>
+
+        <a href="" class="mt-4 px-6 py-2 bg-blue-500 text-white rounded-xl hover:bg-blue-600">Užsakyti</a>
+
       </div>
     </div>
 
